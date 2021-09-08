f885dd66192a0ff7f431c6bc6ce77cd616b0cb1b
Wes <jwesgore@gmail.com>
2021-09-08 18:51:18 -0400

    help me

---
+++ activity-numericalintegration/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <chrono>
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+float f1(float x, int intensity);
+float f2(float x, int intensity);
+float f3(float x, int intensity);
+float f4(float x, int intensity);
+
+#ifdef __cplusplus
+}
+#endif
+
+  
+int main (int argc, char* argv[]) {
+  clock_t tStart = clock();
+  if (argc < 6) {
+    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
+    return -1;
+  }
+  fprintf(stderr, "%ld", tStart);
+  return 0;
+}
