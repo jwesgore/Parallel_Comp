21504950875e0836b6461c08901ff0312497e91b
Wes <jwesgore@gmail.com>
2021-09-09 00:11:07 -0400

    test

--- activity-numericalintegration/main.cpp
+++ activity-numericalintegration/main.cpp
@@ -2,7 +2,10 @@
 #include <cmath>
 #include <cstdlib>
 #include <chrono>
-#include <time.h>
+#include <limits>
+#include <ctime>
+
+using namespace std;
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,11 +22,49 @@ float f4(float x, int intensity);
 
   
 int main (int argc, char* argv[]) {
-  clock_t tStart = clock();
+
+  auto start = std::chrono::system_clock::now();
+
   if (argc < 6) {
     std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
     return -1;
   }
-  fprintf(stderr, "%ld", tStart);
+
+  float vals[argc];
+  auto pFunc = f1;
+
+  for (int i = 0; i < 5; i++) {
+    vals[i] = atoi(argv[i + 1]);
+  }
+
+  float co = (vals[2] - vals[1]) / vals[3];
+  float result;
+
+  switch ((int) vals[0]) {
+    case 1:
+      break;
+    case 2:
+      pFunc = f2;
+      break;
+    case 3:
+      pFunc = f3;
+      break;
+    case 4:
+      pFunc = f4;
+      break;
+  }
+
+  for (int i = 0; i < vals[3]; i++) {
+    float x = vals[1] + (((float) i + .5) * co);
+    result += pFunc(x, vals[4]) * co;
+  }
+
+  auto end = std::chrono::system_clock::now();
+
+  std::chrono::duration<double> diff = end - start;
+
+  //std::cout << result << std::endl;
+  std::cerr << diff.count() << std::endl;
+
   return 0;
 }
