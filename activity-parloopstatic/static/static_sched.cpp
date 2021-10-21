06af463a357ac5130dcf2435238ff48ae9505d84
Wes <jwesgore@gmail.com>
2021-10-21 17:56:20 -0400

    i wanna kill myself

--- activity-parloopstatic/static/static_sched.cpp
+++ activity-parloopstatic/static/static_sched.cpp
@@ -24,8 +24,11 @@ float f4(float x, int intensity);
 
 int main (int argc, char* argv[]) {
 
+  // setup
   auto start = std::chrono::system_clock::now();
-  float result;
+  float result = 0;
+  std::mutex mu;
+  std::vector<std::thread> threads;
   SeqLoop s1;
 
   if (argc < 7) {
@@ -40,8 +43,16 @@ int main (int argc, char* argv[]) {
     vals[i] = atoi(argv[i + 1]);
   }
 
+  // calculate coefficient
   float co = (vals[2] - vals[1]) / vals[3];
   
+  // calculate iter/thread
+  int iterations = (int) vals[3] / (int) vals[5];
+  int overlap = (int) vals[3] % (int) vals[5];
+
+  int start_loop = 0;
+  int end_loop = 0;
+
   // get function
   float (*ptr)(float, int);
   switch ((int) vals[0]) {
@@ -59,11 +70,38 @@ int main (int argc, char* argv[]) {
       break;
   }
 
-  // parloop
-  s1.parfor(0, vals[3], 1, 
-  [&](int i) -> void{
-    result += (*ptr)(vals[1] + ((i + .5) * co), vals[4]) * co;
-    });
+  // threads
+  for (int i = 0; i < vals[5]; i++) {
+    threads.push_back(
+      std::thread([&]() {
+        
+        // split the job evenly
+        mu.lock();
+        start_loop = end_loop;
+        end_loop += iterations;
+        overlap > 0 ? (end_loop++, overlap--) : 0;
+        mu.unlock();
+
+        // parfor
+        s1.parfor<float>(
+          start_loop, end_loop, 1, 
+          [&](float& tls) -> void {
+            tls = 0;
+          },
+          [&](int i, float& tls) -> void { 
+            tls += (*ptr)(vals[1] + ((i + .5) * co), vals[4]) * co;
+          },
+          [&](float& tls) -> void {
+            result += tls;
+          }
+        );
+      }));
+  }
+  
+  // join threads
+  for (auto & t : threads){
+    t.join();
+  }
 
   // get runtime
   auto end = std::chrono::system_clock::now();
