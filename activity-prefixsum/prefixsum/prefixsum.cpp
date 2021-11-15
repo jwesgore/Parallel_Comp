87692a15ea436f8791017d8b422dd0b7b865b939
Wes <jwesgore@gmail.com>
2021-11-15 00:30:21 -0500

    prefix

--- activity-prefixsum/prefixsum/prefixsum.cpp
+++ activity-prefixsum/prefixsum/prefixsum.cpp
@@ -6,6 +6,9 @@
 #include <unistd.h>
 #include "omploop.hpp"
 
+#include <chrono>
+#include <vector>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -17,12 +20,15 @@ extern "C" {
 
 
 int main (int argc, char* argv[]) {
+
+  // start timer
+  auto start = std::chrono::system_clock::now();
+
   if (argc < 3) {
     std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
     return -1;
   }
 
-  
   int n = atoi(argv[1]);
 
   int * arr = new int [n];
@@ -31,10 +37,31 @@ int main (int argc, char* argv[]) {
   int * pr = new int [n+1];
 
   //insert prefix sum code here
+  OmpLoop o1;
+  o1.setNbThread(atoi(argv[2]));
+  o1.parfor<int*>(
+    0,n,1,
+    [&](int* &tls) {
+      tls = new int[n];
+    },
+    [&](int i, int* &tls){
+      
+      for (int j = i; j < n; j++){
+        tls[j] += arr[i];
+      }
+    },
+    [&](int* &tls){
+      for (int j = 0; j < n; j++) pr[j + 1] += tls[j];
+    }
+  );
 
   
-  
   checkPrefixSumResult(pr, n);
+
+  // get runtime
+  auto end = std::chrono::system_clock::now();
+  std::chrono::duration<double> diff = end - start;
+  std::cerr << diff.count() << std::endl;
 
   delete[] arr;
 
