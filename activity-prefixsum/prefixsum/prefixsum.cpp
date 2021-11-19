4feb219e30e6f7538cfd01f89731549744a00752
Wes <jwesgore@gmail.com>
2021-11-18 19:55:34 -0500

    plot

--- activity-prefixsum/prefixsum/prefixsum.cpp
+++ activity-prefixsum/prefixsum/prefixsum.cpp
@@ -34,8 +34,8 @@ int main (int argc, char* argv[]) {
   int * arr = new int [n];
   generatePrefixSumData (arr, n);
 
-  //int * pr = new int [n+1];
-  std::vector<int> pr;
+  int * pr = new int [n+1];
+  //std::vector<int> pr;
   std::vector<std::vector<int>> pr_parts(threads);
 
   //insert prefix sum code here
@@ -44,10 +44,8 @@ int main (int argc, char* argv[]) {
   o1.parfor<std::vector<int>>(
     0,threads, 1,
     [&](std::vector<int> &tls) {
-      tls.push_back(0);
     },
     [&](int i, std::vector<int> &tls){
-      
       tls.push_back(i);
       
       int start = i * (n / threads);
@@ -65,19 +63,45 @@ int main (int argc, char* argv[]) {
     }
   );
 
-  // combine
-  pr.push_back(0);
-  for(int i = 0; i < threads; i++){
-    //calculate fix
-    int fix = pr.back();
 
-    // add elements into pr + fix value
-    for (int j = 2; j < pr_parts[i].size(); j++) {
-      pr.push_back(pr_parts[i][j] + fix);
+  o1.parfor<int> (
+    0, threads, 1,
+    [&](int & tls){
+      tls = 2;
+    },
+    [&](int i, int & tls){
+
+      int fix = 0;
+      for (int j = i; j > 0; j--)
+        fix += pr_parts[j - 1].back();
+
+      int start = i * (n / threads);
+      int end = (i + 1) * (n / threads);
+      
+      if (i == threads -1) end = n;
+
+      for (int j = start; j < end; j++)
+        pr[j + 1] = pr_parts[i][tls++] + fix;
+    },
+    [&](int & tls){
+
     }
-  }
+  );
 
-  checkPrefixSumResult(pr.data(), n);
+  // combine
+  // pr.push_back(0);
+  // for(int i = 0; i < threads; i++){
+  //   //calculate fix
+  //   int fix = pr.back();
+
+  //   // add elements into pr + fix value
+  //   for (int j = 2; j < pr_parts[i].size(); j++) {
+  //     pr.push_back(pr_parts[i][j] + fix);
+  //   }
+  // }
+
+  pr[0] = 0;
+  checkPrefixSumResult(pr, n);
 
   // get runtime
   auto end = std::chrono::system_clock::now();
