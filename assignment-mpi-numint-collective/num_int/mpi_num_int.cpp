3eabe5a6c19f0d8dbc8fc7c720777bce465f65b9
Wes <jwesgore@gmail.com>
2021-11-20 00:17:21 -0500

    fdfd

--- assignment-mpi-numint-collective/num_int/mpi_num_int.cpp
+++ assignment-mpi-numint-collective/num_int/mpi_num_int.cpp
@@ -70,8 +70,9 @@ int main (int argc, char* argv[]) {
   for (int i = loop_start; i < loop_end; i++) {
     rank_val += (*ptr)(a + ((i + .5) * co), intensity);
   }
-  double temp = rank_val;
+  
   rank_val *= co;
+  double temp = rank_val;
   std::cout<< rank << " , " << rank_val <<std::endl;
   MPI_Reduce(&rank_val, &result, size, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
   result += temp;
