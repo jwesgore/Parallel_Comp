b336bb7ca5778cf0f9bb23ddbc0fd3bd60eb4df9
Wes <jwesgore@gmail.com>
2021-11-21 03:18:59 -0500

    gfgfg

--- assignment-mpi-mat-vec/matvec/mpi_matmul.cpp
+++ assignment-mpi-mat-vec/matvec/mpi_matmul.cpp
@@ -135,7 +135,7 @@ int main (int argc, char*argv[]) {
   if (rank_world == 0){
     std::cout << part_len << std::endl;
     for (int i = 0; i < part_len; i++)
-      std::cout << arr_final[i] << " ";
+      std::cout << arr_split[i] << " ";
     std::cout << std::endl;
     // for (int it = 0; it<iter; ++it) {
     //   //matmul(A, x, y, n);
